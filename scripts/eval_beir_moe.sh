693e53e34ecea0d57c5b2c49b7a1731f673e01c6
jordane95 <zehan.li@qq.com>
2024-02-12 02:49:46 +0800

    add scrit for moe

---
+++ scripts/eval_beir_moe.sh
@@ -0,0 +1,13 @@
+
+model_path=$1
+output_dir=$2
+
+
+python eval_beir.py \
+    --task-types "Retrieval" \
+    --output-dir ${output_dir} \
+    --model-name-or-path ${model_path} \
+    --pooling mean \
+    --normalize \
+    --add-pooler moe \
+    --n-experts 8
