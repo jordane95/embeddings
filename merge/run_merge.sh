b047eff73513f0535c01282c71731fe7388df43c
jordane95 <zehan.li@qq.com>
2024-02-12 17:02:51 +0800

    fix bug

--- merge/run_merge.sh
+++ merge/run_merge.sh
@@ -6,4 +6,4 @@ python merge/merge_cqadupstack.py ${result_dir}
 
 python merge/mteb_meta.py ${result_dir} ${save_dir}
 
-python merge_mteb.ppy ${save_dir}/mteb_metadata.md
+python merge/merge_mteb.py --meta-data-path ${save_dir}/mteb_metadata.md
